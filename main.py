6cade568092a48acb911dceb96c00181cb21ca56
sohaib <sohaib1234321@gmail.com>
2024-12-07 21:14:15 +0330

    checking stuf

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ def download_instagram_content(link , tg_id):
         post = instaloader.Post.from_shortcode(loader.context, shortcode)
         loader.filename_pattern = tg_id
         loader.dirname_pattern = f'instadownloads-{tg_id}'
-        bot.send_message(tg_id ,f'is video?:\n{post.is_video}')
+        bot.send_message(tg_id ,f'is video?:\n{post.is_video}\n{post.get_comments()}')
         # دانلود محتوا
         bot.send_message(tg_id,f"Download started...")
         loader.download_post(post, target=content_type)
@@ -170,16 +170,15 @@ def download_instagram_content(link , tg_id):
                 #upload too telegram
         bot.send_message(tg_id , 'uploading to telegram')
         all_in_dir = os.listdir(f"{curent_dir}/instadownloads-{tg_id}/")
-        media_group = []
+
         for item in all_in_dir:
+            
             if item.split('.')[1] != "jpg" :
-                X = InputMediaPhoto(open(f"{curent_dir}/instadownloads-{tg_id}/{item}", 'rb'), caption="عکس اول")
-                media_group.append(X)
+                with open(f"{curent_dir}/instadownloads-{tg_id}/{item}" ,'rb') as ax:
+                    bot.send_photo(tg_id , ax ,caption=ig_caption(tg_id),reply_markup=ig_reply_markup(tg_id))
             if  item.split('.')[1] != "mp4" :
-                Y = InputMediaVideo(open(f"{curent_dir}/instadownloads-{tg_id}/{item}", 'rb'), caption="فیلم اول")
-                media_group.append(Y)
-        
-        bot.send_media_group(tg_id, media_group)
+                with open(f"{curent_dir}/instadownloads-{tg_id}/{item}" ,'rb') as film:
+                    bot.send_video(tg_id , film ,caption=ig_caption(tg_id),reply_markup=ig_reply_markup(tg_id))
     except Exception as e: 
         bot.send_message(tg_id,f"Error downloading link.{e}")
 
